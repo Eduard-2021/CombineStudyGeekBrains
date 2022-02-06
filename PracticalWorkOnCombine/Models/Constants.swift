9f0b02a9d62200b5dda767bc8b83c9387e2d7058
Eduard-2021 <sergsim1978@gmail.com>
2022-02-06 23:15:49 +0200

    Practical task #3 - operators/part2

--- PracticalWorkOnCombine/Models/Constants.swift
+++ PracticalWorkOnCombine/Models/Constants.swift
@@ -13,4 +13,9 @@ class Constants {
     let phoneNumberCodes = [911, 912, 917, 919, 981, 982, 987, 988, 989, 904, 921, 922, 927, 929, 931, 932, 937, 939, 999, 900, 901, 902, 904, 908, 950, 951, 952, 953, 991, 992].map{String($0)}
     let firstNames = ["Олег ","Антон","Петр","Алексей","Игорь","Виталий","Сергей","Владислав", "Евгений"]
     let secondNames = ["Иванов","Петров","Сидоров","Алексеев","Виталиев","Быстрый","Медленный","Светлый", "Темный"]
+    
+    let symbolsForStrings = ["AAAA AAA", "BBBBB BB BBB", "CCC", "DDDDD DD","E EE EEE ","FFF FF F","GGG GGG GGG"]
+    let timeIntervalsForPublisherOfStrings: [TimeInterval] = [0.1, 0.3, 0.2, 0.2, 0.1, 0.3, 0.1]
+    let timeIntervalsForPublisherOfCharacters: [TimeInterval] = [0.1, 0.5, 1.3, 0.7, 1.5, 0.4, 0.1]
+    let emojiRange = [0x1F600, 0x1f601, 0x1f602, 0x1f603, 0x1f604, 0x1f605]
 }
--- PracticalWorkOnCombine/ViewController.swift
+++ PracticalWorkOnCombine/ViewController.swift
@@ -13,8 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
-        practicalWorkNumber_1()
-        practicalWorkNumber_2()
+//        practicalWorkNumber_1()
+//        practicalWorkNumber_2()
+        practicalWorkNumber_3()
     }
     
     let transmittedTextPublisher = PassthroughSubject<String, Never>()
@@ -133,5 +134,122 @@ class ViewController: UIViewController {
             self.searchResultsLabel.text = result
         })
             .store(in: &subscriptions)
+    }
+    
+    //MARK: Practical task #3
+    private func practicalWorkNumber_3(){
+        print("*******  Практическое задание к третьему уроку ********\n")
+        
+        let publisherOfStrings = PassthroughSubject<String, Never>()
+        let publisherOfCharacters = PassthroughSubject<Void, Never>()
+        let publisherСompletedPublisherOfStrings = PassthroughSubject<String, Never>()
+        let publisherСompletedPublisherOfCharacters = PassthroughSubject<String, Never>()
+        let queueForGenerationPush = DispatchQueue(label: "QueueForGenerationPush")
+        let symbolsForStrings = Constants.shared.symbolsForStrings
+        let timeIntervalsForPublisherOfStrings = Constants.shared.timeIntervalsForPublisherOfStrings
+        let timeIntervalsForPublisherOfCharacters = Constants.shared.timeIntervalsForPublisherOfCharacters
+        let emojiRange = Constants.shared.emojiRange
+        var isNeedInsertEmoji = false
+        var resultWorkPublisherOfStrings = ""
+        var resultWorkPublisherOfCharacters = ""
+        
+        //Издатель, который испускает строки, группирует данные каждые 0.5 секунды, сначала преобразует получившиеся массивы в массивы массивов символов типа Unicode.Scalar, потом в массив массивов символов типа Character, после этого преобразует все в массив букв и преобразует его в строку. Строку уже накапливает во внешнем хранилище
+        
+        publisherOfStrings
+            .collect(.byTime(queueForGenerationPush, .seconds(0.5)))
+            .map({stringArray -> [[Unicode.Scalar]] in
+                var unicodeScalarArrays = [[Unicode.Scalar]]()
+                for oneString in stringArray {
+                    let unicodeScalarArray = oneString.unicodeScalars.map{value in
+                        return  Unicode.Scalar(unicodeScalarLiteral: value)
+                    }
+                    unicodeScalarArrays.append(unicodeScalarArray)
+                }
+                return unicodeScalarArrays
+            })
+            .map({unicodeScalarArrays -> [[Character]] in
+                var characterArrays = [[Character]]()
+                for unicodeScalarArray in unicodeScalarArrays {
+                    let characterArray = unicodeScalarArray.map{Character($0)}
+                    characterArrays.append(characterArray)
+                }
+                return characterArrays
+            })
+            .map({characterArrays -> String in
+                let lineInHalfSecond = characterArrays.compactMap({$0}).reduce("", +)
+                return lineInHalfSecond
+            })
+            .sink(receiveCompletion: {_ in
+                publisherСompletedPublisherOfStrings.send(resultWorkPublisherOfStrings)
+            },
+                receiveValue: {
+                resultWorkPublisherOfStrings += $0 + "\n"
+            })
+            .store(in: &subscriptions)
+        
+        
+        //Издатель, который состоит из двух частей - первая имеет подписку, которая меняет флаг isNeedInsertEmoji на true в случае если сообщения не приходят более 0.9 секунды, вторая часть - это подписка, которая реагирует на каждое сообщение и в зависимости от состояния флага добавляет во внешнее хранилище или эмодзи или пустую строку
+        
+        publisherOfCharacters
+            .debounce(for: .seconds(0.9), scheduler: DispatchQueue.main)
+            .share()
+            .sink(receiveCompletion: {_ in
+                publisherСompletedPublisherOfCharacters.send(resultWorkPublisherOfCharacters)
+            },
+                receiveValue: {_ in
+                isNeedInsertEmoji = true
+            })
+            .store(in: &subscriptions)
+        
+        publisherOfCharacters
+            .sink(receiveValue: {
+                if isNeedInsertEmoji {
+                    let emoji = emojiRange[Int.random(in: 0 ... emojiRange.count-1)]
+                    resultWorkPublisherOfCharacters += String(UnicodeScalar(emoji)!)
+                    isNeedInsertEmoji = false
+                }
+                else {
+                    resultWorkPublisherOfCharacters += "\n"
+                }
+            })
+            .store(in: &subscriptions)
+        
+        //Окончательный издатель, который состоит из 2 издателей - первый вызывается когда заканчивает работу издатель строк, второй - издатель эмодзи.
+        
+        publisherСompletedPublisherOfStrings
+            .combineLatest(publisherСompletedPublisherOfCharacters)
+            .map{resultWorkPublisherOfStrings, resultWorkPublisherOfCharacters -> String in
+                let newResultWorkPublisherOfCharacters = resultWorkPublisherOfCharacters.replacingOccurrences(of: "\n", with: "")
+                return "\n" + resultWorkPublisherOfStrings + newResultWorkPublisherOfCharacters
+            }
+            .sink(receiveValue: {result in
+                print(result)
+            })
+            .store(in: &subscriptions)
+        
+        //Блок запуска и завершения работы издателей
+       
+        var timerForPublisherOfStrings: TimeInterval = 0
+        
+        for (index, value) in timeIntervalsForPublisherOfStrings.enumerated() {
+            timerForPublisherOfStrings += value
+            DispatchQueue.main.asyncAfter(deadline: .now() + timerForPublisherOfStrings){
+                publisherOfStrings.send(symbolsForStrings[index])
+            }
+        }
+        
+        var timerForPublisherOfCharacters: TimeInterval = 0
+        
+        for value in timeIntervalsForPublisherOfCharacters {
+            timerForPublisherOfCharacters += value
+            DispatchQueue.main.asyncAfter(deadline: .now() + timerForPublisherOfCharacters){
+                publisherOfCharacters.send()
+            }
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + timerForPublisherOfCharacters + 1){
+            publisherOfStrings.send(completion: .finished)
+            publisherOfCharacters.send(completion: .finished)
+        }
     }
 }
